b8a8176ce508a09a5d9c22f2177d1ad7a56204c5
Evgeny Osatyuk <oswork@ya.ru>
2011-02-18 14:21:43 +0300

    First beta version

---
+++ mrapy.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 Evgeny Osatyuk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from hashlib import md5
+import urllib2
+import httplib
+
+
+try:
+	import json
+	_jloads = lambda s: json.loads(s)
+except ImportError:
+	try:
+		import simplejson
+		_jloads = lambda s: simplejson.loads(s)
+	except ImportError:
+		from django.utils import simplejson
+		_jloads = lambda s: simplejson.loads(s)
+
+def get_uid_by_email( email = None ):
+	"""	
+		Take email as required argument and don't handles any exceptions in case of wrong or missing email.\n
+		Return u'<UID>'.
+	"""
+	email, domain = email.split('@')
+	conn = httplib.HTTPConnection( 'www.appsmail.ru' )
+	conn.request( 'GET', '/platform/%s/%s' % (domain.split('.')[0], email, ) )
+	req = conn.getresponse()
+	if req.status != 200:
+		raise httplib.HTTPException
+	return _jloads( req.read() )['uid']
+
+
+class Mrapy():
+	def __init__( self, app_id, session_key=None, uid=None, secret_key=None, setXML = False ):
+		self._apihost = 'www.appsmail.ru'
+		self._apiurl  = '/platform/api'
+		self.app_id = app_id
+		self.session_key = session_key
+		self.uid = uid
+		self.secret_key = secret_key
+		self.isXML = setXML
+		
+	def sign( self, params ):
+		return md5( "".join(k + "=" + str(params[k]) for k in sorted(params.keys())) + self.secret_key ).hexdigest()
+
+	def tcall( self, method, hash = None ):
+		return self.req_call( method, hash )
+		
+	def call( self, *args, **kwargs ):
+		return self.req_call( args[0], kwargs )
+
+	def req_call( self, method, m_params ):
+
+		if m_params is None:
+			m_params = {}
+		
+		params = {
+			'method': method,
+			'app_id': self.app_id,
+			'secure': '1',
+			'format': 'json'
+		}
+
+		force_uid = None
+		if 'force_uid' in m_params:
+			force_uid = m_params['force_uid']
+			del m_params['force_uid']
+
+		if self.isXML:
+			params.update({ 'format': 'xml' })
+
+		if force_uid or not self.session_key:
+			params.update({ 'uid': force_uid or self.uid })
+		else:
+			params.update({ 'session_key': self.session_key })
+
+		params.update( m_params )
+
+		sig = self.sign( params )
+
+
+		url = ""
+		for k in params:
+			url = url + k + '=' + str(params[k]) + '&'
+		url = url + 'sig=' + sig
+
+		conn = httplib.HTTPConnection( self._apihost )
+		conn.request( 'POST', self._apiurl, url )
+		req = conn.getresponse()
+		if req.status != 200:
+			print "URL: http://%s%s?%s" % ( self._apihost, self._apiurl, url )
+		return _jloads( req.read() )
+
